2d66dae9a77577d9f5d57accd4959fc001f31e4f
surafel21-tech <121454463+surafel21-tech@users.noreply.github.com>
2023-07-25 09:03:38 +0300

    Add files via upload

---
+++ week 2 Quiz App/js/questions.js
@@ -0,0 +1,59 @@
+// Create an array and pass the number, questions, options, and answers
+
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML Stands for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS Stands for?",
+        answer: "Cascade Style Sheet",
+        options: [
+            "Cascade Style Sheet",
+            "Column Style Sheet",
+            "Cascade Style Short",
+            "Column Style Short"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP Stands for?",
+        answer: "Hypertext Preprocessor Language",
+        options: [
+            "Preeprocessor Hypertext Language",
+            "Hypertext Preprocessor Language",
+            "Programmable Hypertext Program",
+            "Hypertext programming Language"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does HTTP Stands for?",
+        answer: "Hypertext Transmission Protocol",
+        options:[
+            "Hypertext Transmission Protocol",
+            "Highcopy Transition Transfer Program",
+            "Hypertext Transfer Protocol",
+            "Hyper Tool Process Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does IP Stands for?",
+        answer: "Internet Protocol",
+        options: [
+            "Internet Protocol",
+            "Intermidate Processor",
+            "Internet Provacy",
+            "Intermidate Protocol"
+        ]
+    },
+];

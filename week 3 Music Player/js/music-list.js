af4e026fd6005cbc750b2611f5f0928013cfbe0b
surafel21-tech <121454463+surafel21-tech@users.noreply.github.com>
2023-07-27 15:34:41 +0300

    Add files via upload

---
+++ week 3 Music Player/js/music-list.js
@@ -0,0 +1,40 @@
+
+let allMusic = [
+  {
+    name: "የ እየሱስ እናት ",
+    artist: "Abel Mekbib ",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "በ ቸርነት፟ _ በአዲስ መልክ የተዘጋጀ",
+    artist: "Abel Mekbib",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "ታላቅ መሆን የፈለገ",
+    artist: "Abel Mekbib",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "የትኛው ስራዪ",
+    artist: "Abel Mekbib",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "ያየህብኝን ያላሳየህብኝ",
+    artist: "Abel Mekbib",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "ይቅር ስላልክኝ",
+    artist: "Abel Mekbib",
+    img: "music-6",
+    src: "music-6"
+  },
+ 
+];
